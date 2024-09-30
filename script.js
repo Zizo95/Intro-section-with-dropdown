17c3c28532c57f5ca1cda4f6c7de6b9586e49b98
Zizo95 <147083219+Zizo95@users.noreply.github.com>
2024-09-30 15:20:54 +0200

    updated js

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 const menu = document.querySelector(".menu");
-const arrowUp = document.querySelector(".icon-arrow-up");
-const arrowDown = document.querySelector(".icon-arrow-down");
+
+const navBar = document.querySelector("nav");
+const login = document.querySelector(".login");
+const register = document.querySelector(".register");
+
+menu.addEventListener("click", function(){
+    menu.style.display = "none"
+navBar.style.display = "flex";
+login.style.display = "flex";
+register.style.display = "flex";
+})
