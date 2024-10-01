ed3b6c1431ecff6b1326a5d2ec43091b073401be
Zizo95 <147083219+Zizo95@users.noreply.github.com>
2024-10-01 15:23:11 +0200

    updated js

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
 const menu = document.querySelector(".menu");
+const closeMenu = document.querySelector(".close-menu");
+const navBar = document.querySelector(".navbar");
+const featureDown = document.querySelector(".fea-arrow-down");
+const featureUp = document.querySelector(".fea-arrow-up");
+const ul = document.querySelector(".list");
+const links = document.querySelector("nav");
 
-const navBar = document.querySelector("nav");
-const login = document.querySelector(".login");
-const register = document.querySelector(".register");
 
 menu.addEventListener("click", function(){
-    menu.style.display = "none"
+menu.style.display = "none"
+closeMenu.style.display = "block";
 navBar.style.display = "flex";
-login.style.display = "flex";
-register.style.display = "flex";
-})
+
+});
+
+closeMenu.addEventListener("click",function(){
+    menu.style.display = "block"
+    closeMenu.style.display = "none";
+    navBar.style.display = "none";
+});
+featureUp.addEventListener("click", function(){
+    event.preventDefault()
+    ul.style.display = "block";
+links.style.lineHeight = "";
+    })
